591c09102e5f906c28499533549ebf9352b92bd9
vmolchanov <vmolchanov@nodasoft.com>
2015-01-16 17:54:38 +0300

    изменена логика запуска тестов

--- TestSuite1.rb
+++ TestSuite1.rb
@@ -1,44 +1,16 @@
-#!/bin/env ruby
-# encoding: utf-8
-
-load 'test1'
-load './test2'
-load './test3-4'
-load './common.functions'
-
-require "selenium-webdriver"
+require 'selenium-webdriver'
 
 #выводим ошибки ruby в файл
-$stderr = File.open("../selenium-webdriver-logs/!errors_log.txt", "w")
+$stderr = File.open('../selenium-webdriver-logs/!errors_log.txt', 'w')
 
-def chooseBrowser(ver)
-@client = Selenium::WebDriver::Remote::Http::Default.new
-@client.timeout = 120 # seconds
-@driver = Selenium::WebDriver.for(:"#{ver}", :http_client => @client)
+require_relative 'test1'
+require_relative 'test2'
+require_relative 'test3-4'
+require_relative 'common.functions'
 
-#лог выполнения тестов
-  $stdout = File.open("../selenium-webdriver-logs/#{ver}_#{date}.txt", "a")
-
-test1()
-
-test2()
-
-#test3()
-
-#test4()
-
-#закрываем файл лога
-  $stdout.flush
-
-@driver.quit
-end
-
-#таймер
+=begin
 loop {
-    #запускаем chrome
-    chooseBrowser('chrome')
-    #запускаем firefox
-    chooseBrowser('firefox')
-    #ждём 1 час
-    sleep 3600
+  #ждём 1 час
+  sleep 3600
 }
+=end
